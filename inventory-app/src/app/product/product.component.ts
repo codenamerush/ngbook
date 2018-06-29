c010b8eade25911f3d4d9a45d46dcfa6d38bb89c
Aarushi <aarushisharda@gmail.com>
2018-06-29 23:04:25 +0530

    fixed html tag issue

--- inventory-app/src/app/product/product.component.ts
+++ inventory-app/src/app/product/product.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-product',
+  selector: 'products-list',
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.css']
 })
