13e9769a793258bcff0282c1407cf6ea6d8c0f9c
Aarushi <aarushisharda@gmail.com>
2018-07-02 19:09:44 +0530

    added css and new component product-row

--- inventory-app/src/app/app.module.ts
+++ inventory-app/src/app/app.module.ts
@@ -2,14 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { ProductComponent } from './product/product.component';
 import { ProductsListComponent } from './products-list/products-list.component';
+import { ProductRowComponent } from './product-row/product-row.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductComponent,
-    ProductsListComponent
+    ProductsListComponent,
+    ProductRowComponent
   ],
   imports: [
     BrowserModule
