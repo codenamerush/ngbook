5eeb87c5684e472af0961ecf5e9bac3ec448eae3
Aarushi <aarushisharda@gmail.com>
2018-07-02 13:59:17 +0530

    changed products list

--- inventory-app/src/app/products-list/products-list.component.ts
+++ inventory-app/src/app/products-list/products-list.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { 
+  Component, 
+  EventEmitter,
+  Input,
+  Output 
+} from '@angular/core';
+import { Product } from '../product.model';
 
+/**
+* @ProductsList: A component for rendering all ProductRows and
+* storing the currently selected Product
+*/
 @Component({
-  selector: 'app-products-list',
-  templateUrl: './products-list.component.html',
-  styleUrls: ['./products-list.component.css']
+  selector: 'products-list',
+  templateUrl: './products-list.component.html'
 })
-export class ProductsListComponent implements OnInit {
+export class ProductsListComponent {
+  /**
+  * @input productList - the Product[] passed to us
+  */
+  @Input() productList: Product[];
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  /**
+  * @output onProductSelected - outputs the current
+  * Product whenever a new Product is selected
+  */
+  @Output() onProductSelected: EventEmitter<Product>;
 }
