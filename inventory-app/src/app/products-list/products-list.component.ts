35dcf32ea1b6014f2c8c5334d239b2097ccf00b2
Aarushi <aarushisharda@gmail.com>
2018-07-02 16:36:25 +0530

    currentProduct added

--- inventory-app/src/app/products-list/products-list.component.ts
+++ inventory-app/src/app/products-list/products-list.component.ts
@@ -1,28 +1,37 @@
-import { 
-  Component, 
+import {
+  Component,
   EventEmitter,
   Input,
-  Output 
+  Output
 } from '@angular/core';
 import { Product } from '../product.model';
 
 /**
-* @ProductsList: A component for rendering all ProductRows and
-* storing the currently selected Product
-*/
+ * @ProductsList: A component for rendering all ProductRows and
+ * storing the currently selected Product
+ */
 @Component({
   selector: 'products-list',
   templateUrl: './products-list.component.html'
 })
 export class ProductsListComponent {
   /**
-  * @input productList - the Product[] passed to us
-  */
+   * @input productList - the Product[] passed to us
+   */
   @Input() productList: Product[];
 
   /**
-  * @output onProductSelected - outputs the current
-  * Product whenever a new Product is selected
-  */
+   * @output onProductSelected - outputs the current
+   *          Product whenever a new Product is selected
+   */
   @Output() onProductSelected: EventEmitter<Product>;
-}
+
+  /**
+   * @property currentProduct - local state containing
+   *             the currently selected `Product`
+   */
+  private currentProduct: Product;
+
+  constructor() {
+    this.onProductSelected = new EventEmitter();
+  }
